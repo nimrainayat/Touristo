ee9c3e2454a38d1bc3a28a31a32bdf147f652852
nimrainayat <69205907+nimrainayat@users.noreply.github.com>
2024-05-17 22:30:11 +0500

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"><br>Touristo . All Rights Reserved <br> </p>
+	</div>
+</div>
